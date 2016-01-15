0f1814625ff94745c6dccf6603cc4e8e99ee5b5c
VEINHORN <b.korogvich@gmail.com>
2016-01-16 01:23:25 +0300

    update ver

--- requester.sh
+++ requester.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -k --request POST -d "xml=<data><name>Markus Right</name><number>1234-5678-9012-3456</number></data>" https://localhost:8443/apiman-gateway/MyOrganization/MyApi/1.03?apikey=16f7be8e-b9df-414c-9c3b-14a5019a1a05
+curl -k --request POST -d "xml=<data><name>Markus Right</name><number>1234-5678-9012-3456</number></data>" https://localhost:8443/apiman-gateway/MyOrganization/MyApi/1.04?apikey=7168de13-3906-4d4b-bf18-219fbfef3733
